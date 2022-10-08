6433bc7ae762af5418bb355b01aefc9d492e24a3
“DML” <dailin1357@gmail.com>
2022-10-09 00:00:14 +0800

    python上传文件到minio测试

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,6 +1,9 @@
+import urllib3
 from django.test import TestCase
 from itsdangerous import URLSafeTimedSerializer
 from itsdangerous import TimestampSigner
+
+from minio import Minio
 
 from django.conf import settings
 
@@ -16,4 +19,21 @@ from django.conf import settings
 
 print('18705171234'[-4])
 obj = {"key1": 1, "key2": 2}
-print(obj.get("key1"), obj.key1)
+
+client = Minio(
+    "192.168.233.5:9000",
+    access_key="admin",
+    secret_key="admin11111",
+    secure=False
+)
+# 判断数据存储的桶是否存在
+found = client.bucket_exists("dltest")
+if not found:
+    client.make_bucket("dltest")
+else:
+    print("Bucket 'dltest' already exists")
+    response = client.get_object("dltest", "20220116_110735.jpg")
+    f = client.fput_object("dltest", "68858462_p0.jpg", r"E:\图片\68858462_p0.jpg")
+    # client.
+    # print(response.data)
+    print(f)
