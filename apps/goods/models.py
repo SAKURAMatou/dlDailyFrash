4bb749a8b63b72665c6666eb06b9b2df03f4bdf1
“DML” <dailin1357@gmail.com>
2022-10-12 23:22:09 +0800

    商品首页数据展示

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -1,3 +1,5 @@
+from tinymce.models import HTMLField
+
 from db.Base_model import *
 
 
@@ -28,7 +30,7 @@ class GoodsSKU(BaseModel):
     stock = models.IntegerField(default=1, verbose_name='库存')
     status = models.SmallIntegerField(default=1, choices=status_choice, verbose_name='商品状态')
     goodType = models.ForeignKey('GoodsType', on_delete=models.DO_NOTHING, verbose_name='商品种类')
-    sealeCount = models.IntegerField(default=0, verbose_name='商品销量')
+    saleCount = models.IntegerField(default=0, verbose_name='商品销量')
     imgGuid = models.ForeignKey('AttachFiles', on_delete=models.DO_NOTHING,
                                 verbose_name='图片id')  # 一个商品可以由多个图片；同一个id关联图片信息表
 
@@ -41,7 +43,8 @@ class GoodsSKU(BaseModel):
 class GoodsSPU(BaseModel):
     '''商品spu表'''
     name = models.CharField(max_length=50, verbose_name="商品spu名称")
-    detail = models.CharField(max_length=500, verbose_name='商品详情')
+    # detail = models.CharField(max_length=500, verbose_name='商品详情')
+    detail = HTMLField(blank=True, verbose_name='商品详情')
 
     class Meta:
         db_table = 'dl_goods_spu'
@@ -58,4 +61,51 @@ class AttachFiles(BaseModel):
     class Meta:
         db_table = 'dl_img_file'
         verbose_name = '图片附件表'
+        verbose_name_plural = verbose_name
+
+
+class IndexGoodsBanner(BaseModel):
+    """首页轮播商品展示模型类"""
+    sku = models.ForeignKey('GoodsSKU', verbose_name='商品', on_delete=models.DO_NOTHING)
+    # image = models.ImageField(upload_to='banner', verbose_name='图片')
+    image = models.ForeignKey('AttachFiles', on_delete=models.DO_NOTHING,
+                              verbose_name='图片id')
+    index = models.SmallIntegerField(default=0, verbose_name='展示顺序')
+
+    class Meta:
+        db_table = 'dl_index_banner'
+        verbose_name = '首页轮播商品'
+        verbose_name_plural = verbose_name
+
+
+class IndexTypeGoodsBanner(BaseModel):
+    '''首页分类商品展示模型类'''
+    DISPLAY_TYPE_CHOICES = (
+        (0, "标题"),
+        (1, "图片")
+    )
+
+    type = models.ForeignKey('GoodsType', verbose_name='商品类型', on_delete=models.DO_NOTHING)
+    sku = models.ForeignKey('GoodsSKU', verbose_name='商品SKU', on_delete=models.DO_NOTHING)
+    display_type = models.SmallIntegerField(default=1, choices=DISPLAY_TYPE_CHOICES, verbose_name='展示类型')
+    index = models.SmallIntegerField(default=0, verbose_name='展示顺序')
+
+    class Meta:
+        db_table = 'dl_index_type_goods'
+        verbose_name = "主页分类展示商品"
+        verbose_name_plural = verbose_name
+
+
+class IndexPromotionBanner(BaseModel):
+    '''首页促销活动模型类'''
+    name = models.CharField(max_length=20, verbose_name='活动名称')
+    url = models.URLField(verbose_name='活动链接')
+    # image = models.ImageField(upload_to='banner', verbose_name='活动图片')
+    image = models.ForeignKey('AttachFiles', on_delete=models.DO_NOTHING,
+                              verbose_name='图片id')
+    index = models.SmallIntegerField(default=0, verbose_name='展示顺序')
+
+    class Meta:
+        db_table = 'df_index_promotion'
+        verbose_name = "主页促销活动"
         verbose_name_plural = verbose_name
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from apps.goods.models import GoodsType
+from django_redis import get_redis_connection
+
+from apps.goods import models as GoodsModels
 
 
 # Create your views here.
@@ -14,9 +16,29 @@ class indexView(View):
 
     def get(self, request):
         # 获取首页商品种类信息
-        goodsType = GoodsType.objects.all()
+        goodsType = GoodsModels.GoodsType.objects.all()
         # 获取首页轮播商品信息
-
+        goodsBanner = GoodsModels.IndexGoodsBanner.objects.all().order_by("index")
         # 获取活动信息
-        # 获取购物车中商品数量
-        return render(request, 'index.html')
+        promotionBanner = GoodsModels.IndexPromotionBanner.objects.all().order_by("index")
+        # 每种商品下的子类
+        # typeGoodsBanner = GoodsModels.IndexTypeGoodsBanner.objects.all()
+        for type in goodsType:
+            textBanner = GoodsModels.IndexTypeGoodsBanner.objects.filter(type=type, display_type=0)
+            imgBanner = GoodsModels.IndexTypeGoodsBanner.objects.filter(type=type, display_type=1)
+            type.textBanner = textBanner
+            type.imgBanner = imgBanner
+        car_count = 5
+        # 判断用户是否登录
+        user = request.user
+        if user.is_authenticated():
+            redisConn = get_redis_connection('default')
+            car_count = redisConn.hlen(f'car_{user.id}')
+
+        context = {
+            'goodsType': goodsType,
+            'goodsBanner': goodsBanner,
+            'promotionBanner': promotionBanner,
+            'car_count': car_count
+        }
+        return render(request, 'index.html', context)
