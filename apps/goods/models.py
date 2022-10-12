a2cbbf90e308be650100bd02f2023fff017c278f
dl <1550288141@qq.com>
2022-10-12 09:43:00 +0800

    商品相关表实体完善

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -4,12 +4,12 @@ from db.Base_model import BaseModel
 
 # Create your models here.
 
-class GoodsType(models):
+class GoodsType(BaseModel):
     '''商品类型模型类'''
     name = models.CharField(max_length=20, verbose_name="种类名称")
     logo = models.CharField(max_length=50, verbose_name="logo图片地址")
     image = models.ImageField(upload_to='type', verbose_name='商品类型图片')
-    imgurl = models.CharField(max_length=100, verbose_name="商品种类图片地址")
+    imgGuid = models.ForeignKey('imgFile', on_delete=models.DO_NOTHING, verbose_name='图片id')  # 一个商品可以由多个图片；同一个id关联图片信息表
 
     class Meta:
         db_table = 'dl_goodsType'
@@ -17,7 +17,7 @@ class GoodsType(models):
         verbose_name_plural = verbose_name
 
 
-class GoodsSKU(models):
+class GoodsSKU(BaseModel):
     '''具体的商品表实体类'''
     status_choice = ((0, '下架'), (1, '上架'))
     priceUnit_choice = ((1, '元'), (2, '美元'), (3, '港币'))
@@ -29,11 +29,17 @@ class GoodsSKU(models):
     status = models.SmallIntegerField(default=1, choices=status_choice, verbose_name='商品状态')
     goodType = models.ForeignKey('GoodsType', on_delete=models.DO_NOTHING, verbose_name='商品种类')
     sealeCount = models.IntegerField(default=0, verbose_name='商品销量')
-    img = models.ForeignKey('imgFile', on_delete=models.DO_NOTHING, verbose_name='图片id')  # 一个商品可以由多个图片；同一个id关联图片信息表
+    imgGuid = models.ForeignKey('imgFile', on_delete=models.DO_NOTHING, verbose_name='图片id')  # 一个商品可以由多个图片；同一个id关联图片信息表
 
 
-class imgFile(models):
+class imgFile(BaseModel):
     '''图片信息表'''
     status_choice = ((0, '下架'), (1, '上架'))
     imgUrl = models.CharField(max_length=100, verbose_name='图片地址')
     status = models.SmallIntegerField(choices=status_choice, default=1)
+
+
+class goodsSPU(BaseModel):
+    '''商品spu表'''
+    name = models.CharField(max_length=50, verbose_name="商品spu名称")
+    detail = models.CharField(max_length=500, verbose_name='商品详情')
